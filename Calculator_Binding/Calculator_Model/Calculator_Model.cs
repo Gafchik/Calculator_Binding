ba7b4254968a8d97c10788cd50ec58a10c1b0859
Dice_kl17 <Dice_kl17@student.itstep.org>
2021-04-14 16:38:03 +0300

    Модель данных создана

---
+++ Calculator_Binding/Calculator_Model/Calculator_Model.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Calculator_Binding.Calculator_Model
+{
+   public class Calculator_Model
+    {
+        public string input_string { get; set; }
+        public double rezult { get; set; }
+    }
+}
