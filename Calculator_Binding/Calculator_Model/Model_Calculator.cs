d7b078cd20c1d24b0a800f225e03d15143f90e0e
Dice_kl17 <Dice_kl17@student.itstep.org>
2021-04-14 16:50:21 +0300

    Основа вью модел готова

--- Calculator_Binding/Calculator_Model/Model_Calculator.cs
+++ Calculator_Binding/Calculator_Model/Model_Calculator.cs
@@ -2,7 +2,7 @@
 
 namespace Calculator_Binding.Calculator_Model
 {
-   public class Calculator_Model
+   public class Model_Calculator
     {
         public string input_string { get; set; }
         public double rezult { get; set; }
---
+++ Calculator_Binding/Calculator_VewModel/VewModel_Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator_Binding.Calculator_Model;
+
+namespace Calculator_Binding.Calculator_VewModel
+{
+    public class VewModel_Calculator : INotifyPropertyChanged
+    {
+        private Model_Calculator model;
+        
+        public Model_Calculator Model
+        {
+            get { return model; }
+            set { model = value; OnPropertyChanged("Model"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+    }
+}
