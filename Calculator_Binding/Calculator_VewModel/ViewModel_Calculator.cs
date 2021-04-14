580c3bda317bae3b76afdf44f4b337753a9f3f04
Dice_kl17 <Dice_kl17@student.itstep.org>
2021-04-14 21:16:12 +0300

    все теперь и с минусом дружит

--- Calculator_Binding/Calculator_VewModel/ViewModel_Calculator.cs
+++ Calculator_Binding/Calculator_VewModel/ViewModel_Calculator.cs
@@ -8,12 +8,15 @@ namespace Calculator_Binding.Calculator_VewModel
     public class ViewModel_Calculator 
     {       
         internal void Calculate(Model_Calculator model)
-        {       
+        {
+            int i = 0;
             bool IS_act = false; // флаг был ли обнаружен оператор
             char act = '+'; // переменная для хранения оператора примера
             string first_number = string.Empty; // первое число 
             string second_number = string.Empty; // второе число
-            for (int i = 0; i < model.Input_string.Length; i++) // цикил бежит по строке и сохраняет цифры и оператор
+            if(model.Input_string[0]== '-')
+            { first_number += '-'; i = 1; }
+            for ( ; i < model.Input_string.Length; i++) // цикил бежит по строке и сохраняет цифры и оператор
             {
                 switch (model.Input_string[i])
                 {
@@ -42,40 +45,56 @@ namespace Calculator_Binding.Calculator_VewModel
                         break;
                 }               
             }
-            switch (act) // свич для выбора мат действия и присваивание результатов
+            try
             {
-                case '+':
-                    model.Rezult = double.Parse(first_number) + double.Parse(second_number);
-                    break;
-                case '-':
+                switch (act) // свич для выбора мат действия и присваивание результатов
+                {
 
-                    model.Rezult = double.Parse(first_number) - double.Parse(second_number);
-                    break;
-                case '*':
-                    model.Rezult = double.Parse(first_number) * double.Parse(second_number);
-                    break;
-                case '/':
-                    if (double.Parse(second_number) != 0)
-                        model.Rezult = double.Parse(first_number) / double.Parse(second_number);
-                    else
-                        throw new DivideByZeroException();
-                    break;
-                default:
-                    break;
-            }            
-            model.Last_line = model.Input_string;
-            model.Input_string = model.Rezult.ToString();
+                    case '+':
+                        model.Rezult = double.Parse(first_number) + double.Parse(second_number);
+                        break;
+                    case '-':
+                        model.Rezult = double.Parse(first_number) - double.Parse(second_number);
+                        break;
+                    case '*':
+                        model.Rezult = double.Parse(first_number) * double.Parse(second_number);
+                        break;
+                    case '/':
+                        if (double.Parse(second_number) != 0)
+                            model.Rezult = double.Parse(first_number) / double.Parse(second_number);
+                        else
+                            throw new DivideByZeroException();
+                        break;
+                    default:
+                        break;
+                }
+                model.Last_line = model.Input_string;
+                model.Input_string = model.Rezult.ToString();
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("На 0 делить нельзя", "Дибил", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Что-то пошло не так", "Дибил", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+                       
+            
         }
 
         internal  void Input(object sender, Model_Calculator model)
         {
-            var key = (sender as Button);
-            try
-            {
+            var key = (sender as Button);           
                 switch (key.Content.ToString())
                 {
                     case "+":
-                        if (Find_op(model))
+                         if (model.Input_string[0] == '-')
+                            model.Input_string += key.Content.ToString();
+                        else if (Find_op(model))
                             Calculate(model);
                         else
                             model.Input_string += key.Content.ToString();
@@ -83,17 +102,23 @@ namespace Calculator_Binding.Calculator_VewModel
                     case "-":
                         if (Find_op(model))
                             Calculate(model);
+                        else if (model.Input_string == "")
+                            model.Input_string = "0-";
                         else
                             model.Input_string += key.Content.ToString();
                         break;
                     case "*":
-                        if (Find_op(model))
+                        if (model.Input_string[0] == '-')
+                            model.Input_string += key.Content.ToString();
+                        else if (Find_op(model))
                             Calculate(model);
                         else
                             model.Input_string += key.Content.ToString();
                         break;
                     case "/":
-                        if (Find_op(model))
+                        if (model.Input_string[0] == '-')
+                            model.Input_string += key.Content.ToString();
+                        else if (Find_op(model))
                             Calculate(model);
                         else
                             model.Input_string += key.Content.ToString();
@@ -115,14 +140,7 @@ namespace Calculator_Binding.Calculator_VewModel
                     default:
                         model.Input_string += key.Content.ToString();
                         break;
-                }
-            }
-            catch(DivideByZeroException)
-            {
-                MessageBox.Show("На 0 делить нельзя", "Дибил", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
+                }                    
         }
         private bool Find_op(Model_Calculator model)
         {
